d5553d020c0c40d75370de74fa30f0653fbedc52
Josleee <cz2f@qq.com>
2017-06-16 00:16:28 +0200

    Refine config.

--- network_tools/config.py
+++ network_tools/config.py
@@ -1,8 +1,15 @@
 headers = {
-    'headers': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
+    'headers': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) '
+               'Chrome/57.0.2987.133 Safari/537.36'}
 
-interval_sleep_time = 0.1
-try_times = 3
+# Request send interval time
+execute_interval_time = 0.1
+# After failure wait time
+fail_wait_time = 5
+# After failure try times
+try_times = 5
 
-# boxoffice can be changed to 'top'
-grab_list_category = 'boxoffice'
+# Box Office, Most Popular Movies, Top Rated Movies, Top Rated English Movies, Lowest Rated Movies
+chart_category = ['boxoffice', 'moviemeter', 'top', 'top-english-movies', 'bottom']
+# 0 ~ 4 corresponding to description above
+category_selector = 1
